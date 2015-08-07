844818e2dbe60243f24a445bef2310b902220f49
Rajesh Babu <trcrajesh_be@yahoo.com>
2015-08-07 14:15:11 -0400

    Create widget.test.js

---
+++ UnitTesting_with_injection/widget.test.js
@@ -0,0 +1,11 @@
+ /*!
+  * @Project Name			: New 
+  * @Description			: This is section is for unit test cases for accordion
+  * @Created				  : Rajesh Thoghuluva - Aug 07, 2015
+  * @Last Modified 			: <Enter Here>
+  */
+"use strict";
+
+QUnit.test( "hello test", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
+});
